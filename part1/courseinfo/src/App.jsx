3db04936e0e2a1436bb5d69d98c75f9a16959a9f
Johannes Vihavainen <johannesvihavainen@outlook.com>
2025-11-26 15:18:28 +0200

    course information 2.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,6 +18,7 @@ const Content = (props) => {
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
       <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
       <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part part={props.parts[3].name} exercises={props.parts[3].exercises} />
     </div>
   )
 }
@@ -36,7 +37,7 @@ const Course = ({ course }) => {
 
 const Total = (props) => {
   return <div>
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <p><strong>total of {props.parts.reduce((accumulator, part) => accumulator + part.exercises, 0)} exercises</strong></p>
   </div>
 }
 
@@ -59,6 +60,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
