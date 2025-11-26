e0d7e6972594b41789c8cff66d98258ad99a0fd0
Johannes Vihavainen <johannesvihavainen@outlook.com>
2025-11-26 15:00:45 +0200

    course information 2.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -22,34 +22,7 @@ const Content = (props) => {
   )
 }
 
-
-
-const Total = (props) => {
-  return <div>
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  </div>
-}
-
-function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
@@ -59,4 +32,40 @@ function App() {
   )
 }
 
+
+
+const Total = (props) => {
+  return <div>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  </div>
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
 export default App
+
+
